4bcd17059e156145f6a1aff17494e511e88af25a
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-05-15 10:43:02 -0300

    Anotações

---
+++ src/Declaracao_Variaveis_Tipo_Numeros_Inteiros.java
@@ -0,0 +1,13 @@
+public class Declaracao_Variaveis_Tipo_Numeros_Inteiros {
+    public static void main(String[] args) {
+
+        byte anos_Trabalhados = 65; //Declaração de variável tipo byte.
+        short pontos_Acumulados = 12_450; //Declaração de variável tipo short.
+        int pontos_Totais = 1_456_543_983; //Declaração de variável tipo int.
+        long contribuicao_Equipe = 231_431_456_543_983L; //Declaração de variável tipo long.
+
+        System.out.printf("\nAnos trabalhados = %d\nPontos acumulados = %d\nPontos totais = %d\nContribuição da Equipe = %d\n",
+                anos_Trabalhados, pontos_Acumulados, pontos_Totais, contribuicao_Equipe);
+
+    }
+}
