aecb03cbe3b229c666010836898c0a4b3f4d71bd
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-05-15 10:54:47 -0300

    Anotações

---
+++ src/Declaracao_Variaveis_Tipe_Real.java
@@ -0,0 +1,10 @@
+public class Declaracao_Variaveis_Tipe_Real {
+    public static void main(String[] args) {
+
+        float desconto_beneficios = 65.76F; //Declaração de variável tipo float.
+        double salario_funcionario = 8_450.34; //Declaração de variável tipo double.
+
+        System.out.printf("\nSalario do funcionário: R$%.2f\nValor descontado = R$%.2f\n",
+                salario_funcionario, desconto_beneficios);
+    }
+}
