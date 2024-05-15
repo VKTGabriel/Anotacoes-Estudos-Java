dff98e921079840a26fc7cc5a1fbf26fb70b2f38
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-05-15 12:44:28 -0300

    Anotações

---
+++ src/Declaracao_Variaveis_Tipo_Caractere_e_Logico.java
@@ -0,0 +1,10 @@
+public class Declaracao_Variaveis_Tipo_Caractere_e_Logico {
+    public static void main(String[] args) {
+
+        char genero_Funcionario = 'F'; //Declaração de variável tipo char (Caractere).
+        boolean aptidao = true; //Declaração de variável tipo boolean (Lógico).
+
+        System.out.printf("\nGênero do funcionário: %c\nApto = %b\n",
+                genero_Funcionario, aptidao);
+    }
+}
