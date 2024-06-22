8fabe6916a52da1a59fccb45f0291e00f36a4121
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-06-21 22:21:56 -0300

    Anotações

---
+++ src/Exercicios/Classes/Matematico/Matematica_Vetor.java
@@ -0,0 +1,41 @@
+package Exercicios.Classes.Matematico;
+
+public class Matematica_Vetor {
+    int maiorEmVetor (int[] a) {
+        int maior = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (maior < a[i]) {
+                maior = a[i];
+            }
+        }
+        return maior;
+    }
+
+    int menorEmVetor (int[] a) {
+        int menor = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (menor > a[i]) {
+                menor = a[i];
+            }
+        }
+        return menor;
+    }
+
+    int somaEmVetor (int[] a) {
+        int soma = 0;
+        for (int i = 0; i < a.length; i++) {
+                soma += a[i];
+        }
+        return soma;
+    }
+
+    double mediaEmVetor (int[] a) {
+        double soma = 0;
+        for (int i = 0; i < a.length; i++) {
+            soma += a[i];
+        }
+        return soma/a.length;
+    }
+
+}
+
---
+++ src/Exercicios/Classes/Matematico/Matematica_Vetor_Teste.java
@@ -0,0 +1,31 @@
+package Exercicios.Classes.Matematico;
+
+import javax.swing.*;
+
+public class Matematica_Vetor_Teste {
+    public static void main(String[] args) {
+        Matematica_Vetor m = new Matematica_Vetor();
+
+        int valor = Integer.parseInt(JOptionPane.showInputDialog("Quantos número deseja analizar?"));
+        int[] n = new int[valor];
+
+        for (int i = 0; i < valor; i++) {
+            n[i] = Integer.parseInt(JOptionPane.showInputDialog("[Faltam " + (valor - i) + "] Digite um valor?"));
+        }
+
+        int menor = m.menorEmVetor(n);
+        int maior = m.maiorEmVetor(n);
+        int soma = m.somaEmVetor(n);
+        double media = m.mediaEmVetor(n);
+        String print_Array = String.format("Array: %d", n[0]), saida = "";
+
+        for (int i = 1; i < n.length; i++) {
+            print_Array += ", " + n[i];
+        }
+
+        saida = String.format("%s\nMenor Valor = %d\nMaior Valor = %d\nSoma dos valores = %d\nMedia = %.2f\n",
+                print_Array, menor, maior, soma, media);
+        JOptionPane.showMessageDialog(null, saida);
+
+    }
+}
